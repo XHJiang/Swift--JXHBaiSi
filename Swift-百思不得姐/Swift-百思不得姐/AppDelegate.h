a8448d1141c4fa6a34a3eb63045b651ee3959a9b
JXH <917532427@qq.com>
2016-07-06 11:39:15 +0800

    初始化项目

---
+++ Swift-百思不得姐/Swift-百思不得姐/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Swift-百思不得姐
+//
+//  Created by mac on 16/7/6.
+//  Copyright © 2016年 JXH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Swift-百思不得姐/Swift-百思不得姐/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Swift-百思不得姐
+//
+//  Created by mac on 16/7/6.
+//  Copyright © 2016年 JXH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
